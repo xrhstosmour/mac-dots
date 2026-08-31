cdc4baf088cfdd4a8d4f9ad0f81bca1baa399403
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2026-08-31 23:32:46 +0300

    Show Nerd Font icon in place of tab process name

--- .config/wezterm/tabs.lua
+++ .config/wezterm/tabs.lua
@@ -1,6 +1,74 @@
 local wezterm = require 'wezterm'
 local constants = require 'constants'
 
+-- Nerd Font icons shown in place of the process name when a match exists.
+-- WezTerm cannot resolve arbitrary process names to icons automatically, glyphs only exist for
+-- tools Nerd Fonts has curated, so unmapped processes (e.g. `claude`, `htop`, `curl`) fall back
+-- to plain text.
+local process_icons = {
+    ["git"] = wezterm.nerdfonts.dev_git,
+    ["gh"] = wezterm.nerdfonts.dev_github,
+    ["tmux"] = wezterm.nerdfonts.cod_terminal_tmux,
+    ["vim"] = wezterm.nerdfonts.dev_vim,
+    ["nvim"] = wezterm.nerdfonts.custom_neovim,
+    ["node"] = wezterm.nerdfonts.dev_nodejs,
+    ["python"] = wezterm.nerdfonts.dev_python,
+    ["python3"] = wezterm.nerdfonts.dev_python,
+    ["docker"] = wezterm.nerdfonts.dev_docker,
+    ["docker-compose"] = wezterm.nerdfonts.dev_docker,
+    ["cargo"] = wezterm.nerdfonts.dev_rust,
+    ["rustc"] = wezterm.nerdfonts.dev_rust,
+    ["go"] = wezterm.nerdfonts.dev_go,
+    ["ruby"] = wezterm.nerdfonts.dev_ruby,
+    ["ssh"] = wezterm.nerdfonts.dev_ssh,
+    ["bash"] = wezterm.nerdfonts.dev_bash,
+    ["zsh"] = wezterm.nerdfonts.dev_zsh,
+    ["fish"] = wezterm.nerdfonts.dev_fish,
+    ["java"] = wezterm.nerdfonts.dev_java,
+    ["php"] = wezterm.nerdfonts.dev_php,
+    ["npm"] = wezterm.nerdfonts.dev_npm,
+    ["yarn"] = wezterm.nerdfonts.dev_yarn,
+    ["kubectl"] = wezterm.nerdfonts.dev_kubernetes,
+    ["terraform"] = wezterm.nerdfonts.dev_terraform,
+    ["psql"] = wezterm.nerdfonts.dev_postgresql,
+    ["mysql"] = wezterm.nerdfonts.dev_mysql,
+    ["redis-cli"] = wezterm.nerdfonts.dev_redis,
+    ["brew"] = wezterm.nerdfonts.dev_homebrew,
+    ["code"] = wezterm.nerdfonts.dev_vscode,
+    ["subl"] = wezterm.nerdfonts.dev_sublime,
+    ["emacs"] = wezterm.nerdfonts.dev_emacs,
+    ["nano"] = wezterm.nerdfonts.dev_nano,
+    ["sqlite3"] = wezterm.nerdfonts.dev_sqlite,
+    ["cmake"] = wezterm.nerdfonts.dev_cmake,
+    ["aws"] = wezterm.nerdfonts.dev_aws,
+    ["gcloud"] = wezterm.nerdfonts.dev_googlecloud,
+    ["ansible"] = wezterm.nerdfonts.dev_ansible,
+    ["ansible-playbook"] = wezterm.nerdfonts.dev_ansible,
+    ["vagrant"] = wezterm.nerdfonts.dev_vagrant,
+    ["helm"] = wezterm.nerdfonts.dev_helm,
+    ["deno"] = wezterm.nerdfonts.dev_denojs,
+    ["bun"] = wezterm.nerdfonts.dev_bun,
+    ["perl"] = wezterm.nerdfonts.dev_perl,
+    ["lua"] = wezterm.nerdfonts.dev_lua,
+    ["zig"] = wezterm.nerdfonts.dev_zig,
+    ["mvn"] = wezterm.nerdfonts.dev_maven,
+    ["gradle"] = wezterm.nerdfonts.dev_gradle,
+    ["composer"] = wezterm.nerdfonts.dev_composer,
+    ["poetry"] = wezterm.nerdfonts.dev_poetry,
+    ["conda"] = wezterm.nerdfonts.dev_anaconda,
+    ["elixir"] = wezterm.nerdfonts.dev_elixir,
+    ["iex"] = wezterm.nerdfonts.dev_elixir,
+    ["erl"] = wezterm.nerdfonts.dev_erlang,
+    ["swift"] = wezterm.nerdfonts.dev_swift,
+    ["dart"] = wezterm.nerdfonts.dev_dart,
+    ["dotnet"] = wezterm.nerdfonts.dev_dotnet,
+    ["gcc"] = wezterm.nerdfonts.dev_c,
+    ["clang"] = wezterm.nerdfonts.dev_c,
+    ["cc"] = wezterm.nerdfonts.dev_c,
+    ["g++"] = wezterm.nerdfonts.dev_cplusplus,
+    ["c++"] = wezterm.nerdfonts.dev_cplusplus
+}
+
 -- Define the function that applies tabs configuration.
 local function apply_configuration(configuration)
     configuration.use_fancy_tab_bar = true
@@ -56,7 +124,7 @@ local function apply_configuration(configuration)
                 end
             end
 
-            local title = process_name
+            local title = process_icons[process_name] or process_name
             if path_name then
                 title = title .. " · " .. path_name
             end
